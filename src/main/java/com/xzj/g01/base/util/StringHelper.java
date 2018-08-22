d8f857710b061c528641cfb5b395339b341f6dec
xiezhijun <1273440584@qq.com>
2018-08-22 15:46:52 +0800

    String工具类

---
+++ src/main/java/com/xzj/g01/base/util/StringHelper.java
@@ -0,0 +1,43 @@
+package com.xzj.g01.base.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/***
+ * String工具类
+ * @author Zane
+ * @Time 2018年8月15日 上午10:21:30
+ */
+public class StringHelper {
+	
+	/**
+     * 过滤emoji 或者 其他非文字类型的字符
+     * @param source
+     * @return
+     */
+    public static String filterEmoji(String source) 
+    {
+ 
+    	 if (source==null || source.length()==0) 
+         {
+             return "";
+         }
+    	 
+    	Pattern emoji = Pattern.compile (
+				"[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]|[\ue000-\uefff]" ,
+		        Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE );
+		
+	 	
+		Matcher emojiMatcher = emoji.matcher(source);
+
+	    if( emojiMatcher.find()) 
+	    {
+	    	String no_emoji = emojiMatcher.replaceAll("*");
+	    	return no_emoji;
+	    }
+	    
+	    return source;
+ 
+    }
+     
+}
