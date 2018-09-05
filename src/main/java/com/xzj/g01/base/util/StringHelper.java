20d4bff541ce177fb629bf996f6e0dd9aac6f4b9
xiezhijun <1273440584@qq.com>
2018-09-05 16:16:13 +0800

    工具类添加过滤Emoji

--- src/main/java/com/xzj/g01/base/util/StringHelper.java
+++ src/main/java/com/xzj/g01/base/util/StringHelper.java
@@ -15,29 +15,44 @@ public class StringHelper {
      * @param source
      * @return
      */
-    public static String filterEmoji(String source) 
-    {
- 
-    	 if (source==null || source.length()==0) 
-         {
-             return "";
-         }
-    	 
-    	Pattern emoji = Pattern.compile (
-				"[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]|[\ue000-\uefff]" ,
-		        Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE );
-		
-	 	
-		Matcher emojiMatcher = emoji.matcher(source);
+	public static String filterEmoji(String source) {
+		if (source == null || source.length() == 0) {
+			return "";
+		}
+		Pattern emoji = Pattern.compile(
+				"[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]|[\ue000-\uefff]",
+				Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
 
-	    if( emojiMatcher.find()) 
-	    {
-	    	String no_emoji = emojiMatcher.replaceAll("*");
-	    	return no_emoji;
-	    }
-	    
-	    return source;
- 
-    }
+		Matcher emojiMatcher = emoji.matcher(source);
+		if (emojiMatcher.find()) {
+			String no_emoji = emojiMatcher.replaceAll("*");
+			return no_emoji;
+		}
+		return source;
+
+	}
+    
+    /**
+     * 检测是否有emoji字符
+     * @param source
+     * @return 一旦含有就抛出
+     */
+	public static boolean containsEmoji(String source) {
+		if (source == null || source.length() == 0) {
+			return false;
+		}
+		//
+		// 过滤表情字符
+		Pattern emoji = Pattern.compile(
+				"[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]|[\ue000-\uefff]",
+				Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
+
+		Matcher emojiMatcher = emoji.matcher(source);
+		if (emojiMatcher.find()) {
+			return true;
+		}
+		return false;
+
+	}
      
 }
